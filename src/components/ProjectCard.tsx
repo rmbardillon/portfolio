24853fb9b6ac5cc3718ace5e21eba7d3bbcb2a71
Romeo Jr Montealegre Bardillon <107736688+rmbardillon@users.noreply.github.com>
2023-09-19 20:45:00 +0800

    Added modal in project images

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Slider from "react-slick";
+import Modal from "react-modal";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 interface ProjectCardProps {
 	name: string;
 	description: string;
@@ -18,27 +21,37 @@ const ProjectCard = ({
 	code,
 	stack,
 }: ProjectCardProps) => {
-    const settings = {
+	const settings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		autoplay: true, // Set autoplay to true
-		autoplaySpeed: 3000, // Set your preferred autoplay speed (in milliseconds)
+		autoplay: true,
+		autoplaySpeed: 3000,
 	};
+
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+	const openModal = (index: number) => {
+		setSelectedImageIndex(index);
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<div className="col" id="project">
 			<div
 				className="card"
-				style={{
-					height: "500px",
-					overflow: "hidden",
-				}}
+				style={{ height: "500px", overflow: "hidden" }}
 			>
 				<Slider {...settings}>
 					{images.map((image, index) => (
-						<div key={index}>
+						<div key={index} onClick={() => openModal(index)}>
 							<img
 								className="card-img-top w-100 d-block fit-cover"
 								style={{ height: "200px" }}
@@ -79,6 +92,37 @@ const ProjectCard = ({
 					)}
 				</div>
 			</div>
+			<Modal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				style={{
+					overlay: {
+						backgroundColor: "rgba(0, 0, 0, 0.8)",
+					},
+					content: {
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						width: "70vw",
+                        height: "80vh",
+                        maxWidth: "90vw",
+                        maxHeight: "80vh",
+						overflow: "hidden",
+					},
+				}}
+			>
+				<Slider {...settings} initialSlide={selectedImageIndex}>
+					{images.map((image, index) => (
+						<div key={index}>
+							<img
+								src={image}
+								alt={name}
+								style={{ width: "100%" }}
+							/>
+						</div>
+					))}
+				</Slider>
+			</Modal>
 		</div>
 	);
 };
